171615a06b710c3625d8ccf2a0088deb163f8435
OuKeiZee <oukeizee@gmail.com>
2026-09-17 00:13:56 -0500

    Actualizar recursos de imágenes de Fresatto

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
   const BASE_SCRIPT="js/script-base.js";
   const CUSTOM_SCRIPT="js/product-configurator.js";
   const FIX_SCRIPT="js/fresatto-configurator-fix.js";
-  const ASSET_VERSION="20260917-2";
+  const ASSET_VERSION="20260917-3";
   const OPEN_HOUR=10;
   const CLOSE_HOUR=22;
   const FRESATTO_NAME="Fresatto";
